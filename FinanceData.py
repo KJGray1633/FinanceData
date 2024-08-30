4f9b1445e770fded7e1e5952463271f48ec7c317
Graybill <graybillkb20@gcc.edu>
2024-08-30 15:32:48 -0400

    Added documentation to functions in FinanceData class

--- FinanceData.py
+++ FinanceData.py
@@ -3,7 +3,18 @@ from FinanceDataHelper import FinanceDataHelper
 
 class FinanceData(object):
     @staticmethod
-    def get_data(save_to_csv: bool = True, file_path_name: str | None = None, most_recent_day: bool = True) -> pd.DataFrame | None:
+    def get_data(save_to_csv: bool = True, file_path_name: str | None = None, most_recent_day: bool = True) -> pd.DataFrame:
+        """
+        Gets data data from S&P 500 for about the past month through Yahoo Finance API
+
+        Args:
+            save_to_csv: if you want the data to also be saved as a csv -> bool
+            file_path_name: the relative path and file name (only needed if save_to_csv is true) -> str
+            most_recent_day: if you want data from the most recent day in order to make predictions -> bool
+
+        Returns:
+            The resulting Pandas DataFrame with S&P 500 data for about the past month -> pandas.DataFrame
+        """
         if save_to_csv and file_path_name is None:
             raise ValueError('Must include a file path (including name) if saving to csv')
         if file_path_name is not None and file_path_name[-4:] != '.csv':
@@ -25,11 +36,20 @@ class FinanceData(object):
         if save_to_csv and combined_df is not None:
             combined_df.to_csv(file_path_name) 
 
-        return combined_df
+        return combined_df #type: ignore
     
     @staticmethod
-    def get_data_from_csv(file_path: str) -> pd.DataFrame:
-        df = pd.read_csv(file_path).set_index(["Symbol","Date"])
+    def get_data_from_csv(file_path_name: str) -> pd.DataFrame:
+        """
+        Gets data data from S&P 500 for about the past month from a saved .csv file
+
+        Args:
+            file_path_name: the relative path and file name of the saved .csv file-> str
+
+        Returns:
+            The resulting Pandas DataFrame with S&P 500 data for about the past month -> pandas.DataFrame
+        """
+        df = pd.read_csv(file_path_name).set_index(["Symbol","Date"])
         return df
     
 
